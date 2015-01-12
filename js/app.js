a976e39d2b934486a52342b877a7849cb6f2f433
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-11 22:38:04 -0800

    add audio function

--- js/app.js
+++ js/app.js
@@ -27,4 +27,10 @@ $(document).ready(function() {
     	$('.ryu-throwing').hide();
   		$('.ryu-ready').show();
   	});
-})
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
