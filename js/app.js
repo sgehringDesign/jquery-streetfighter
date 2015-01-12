88ded631f4e8be130cb0f112ed37e6d4bae8831e
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-11 23:57:11 -0800

    add ryu dancing

--- js/app.js
+++ js/app.js
@@ -1,18 +1,36 @@
+
+
+// NOTICED IN ORGINIAL "var hadoukenSound = false;" is global
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+function resetRyu() {
+	$('.ryu-still').hide();
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-cool').hide();
+}
+
+
 $(document).ready(function() {
 
 	$('.ryu').mouseenter(function() {
-    	$('.ryu-still').hide();
+    	resetRyu();
 		$('.ryu-ready').show();
   	})
   	.mouseleave(function() {
-		$('.ryu-ready').hide();
+		resetRyu();
     	$('.ryu-still').show();
   	})
   	.mousedown(function() {
     	playHadouken(); 
-    	$('.ryu-ready').hide();
+    	resetRyu();
     	$('.ryu-throwing').show();
-    	$('.hadouken').show()
+    	$('.hadouken').show();
     	$('.hadouken').finish().show()
     	.animate(
     		{'left': '300px'}, 
@@ -24,26 +42,21 @@ $(document).ready(function() {
     	);
 	})
   	.mouseup(function() {
-    	$('.ryu-throwing').hide();
+    	resetRyu();
   		$('.ryu-ready').show();
   	})
   	
-  	$('body').keydown(function(e) {
+  	$(document).keydown(function(e) {
   		if(e.keyCode === 88) {
-  			$('.ryu-still').hide();
+  			resetRyu();
     		$('.ryu-cool').show();
   		}
 	})
 	.keyup(function(e) {
 		if(e.keyCode === 88) {
-    		$('.ryu-cool').hide();
+    		resetRyu();
     		$('.ryu-still').show();
   		}
 	});
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
