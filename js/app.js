68eda3c579b7abcdb12126af8aea54a959d783ce
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-11 22:12:46 -0800

    added mouse leave event handling

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
 	$('.ryu').mouseenter(function() {
     	$('.ryu-still').hide();
 		$('.ryu-ready').show();
+  	})
+  	.mouseleave(function() {
+		$('.ryu-ready').hide();
+    	$('.ryu-still').show();
   	});
 })
