3f13020b2b836faa41a0b49df545290fa2270fce
Stephen Gehring <sgehringdesign@gmail.com>
2015-01-12 00:58:45 -0800

    fixing tab weirdness

--- js/app.js
+++ js/app.js
@@ -1,77 +1,55 @@
 
-
 // NOTICED IN ORGINIAL "var hadoukenSound = false;" is global
-
 function playHadouken() {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
 
-
 $(document).ready(function() {
-
-
-  // RYU EVENTS
+	// RYU EVENTS
 	$('.ryu').mouseenter(function() {
-    $('.ryu-action').hide();
-		$('.ryu-ready').show();
-    $('.ryu').data("state", "ready");
-		console.log("enter");
-  })
-  .mouseleave(function() {
 		$('.ryu-action').hide();
-    $('.ryu-still').show();
-    $('.ryu').data("state", "still");
-    console.log("leave");
-  })
-  .mousedown(function() {
-    playHadouken(); 
-    $('.ryu-action').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show();
-    $('.hadouken').finish().show()
-    .animate(
-    	{'left': '300px'}, 
-    	500,
-    	function() {
-    			$(this).hide();
-    			$(this).css('left', '-200px');
-    	}
-    );
+		$('.ryu-ready').show();
+		$('.ryu').data("state", "ready");
 	})
-  .mouseup(function() {
-    $('.ryu-action').hide();
-  	$('.ryu-ready').show();
-    console.log("up");
-  })
-  	
+	.mouseleave(function() {
+		$('.ryu-action').hide();
+		$('.ryu-still').show();
+		$('.ryu').data("state", "still");
+	})
+	.mousedown(function() {
+		playHadouken(); 
+		$('.ryu-action').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		$('.hadouken').finish().show()
+		.animate({'left': '300px'}, 500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-200px');
+		});
+	})
+	.mouseup(function() {
+		$('.ryu-action').hide();
+		$('.ryu-ready').show();
+	})
 
-  // KEY EVENTS
-  $(document).keydown(function(e) {
-  	if(e.keyCode === 88) {
-  		$('.ryu-action').hide();
-    	$('.ryu-cool').show();
-    	console.log("x key down");
-  	}
+	// KEY EVENTS
+	$(document).keydown(function(e) {
+		if(e.keyCode === 88) {
+			$('.ryu-action').hide();
+			$('.ryu-cool').show();
+		}
 	})
 	.keyup(function(e) {
-
-    console.log("x key up");
-
 		if(e.keyCode === 88) {
-
-    	$('.ryu-action').hide();
-      
-      if($('.ryu').data("state") == "ready") {
-        $('.ryu-ready').show();
-        return;
-      }
-    	$('.ryu-still').show();
-  	}
-    
+			$('.ryu-action').hide();
+			if($('.ryu').data("state") === "ready") {
+				$('.ryu-ready').show();
+				return;
+			}
+			$('.ryu-still').show();
+		}
 	});
-
-
 });
-
